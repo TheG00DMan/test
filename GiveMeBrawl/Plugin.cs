670b6aebd0eb0f0ab346d055a4d0b1e79cd72fc7
Classic <Classic@DESKTOP-EAT4OHI>
2024-07-27 20:50:31 +0100

    Add project files.

---
+++ GiveMeBrawl/Plugin.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx;
+using Utilla;
+
+namespace GiveMeBrawl
+{
+    [ModdedGamemode("PAINTBRAWL","PAINT BRAWL", Utilla.Models.BaseGamemode.Battle)]
+    [BepInDependency("org.legoandmars.gorillatag.utilla")]
+    [BepInPlugin("GiveMeBrawl", "Give Me Brawl", "1.0.0")]
+    public class Plugin : BaseUnityPlugin{}
+}
